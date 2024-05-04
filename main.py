754ff86418d021a3b98795872790f63bd32ef0f3
HunarPG <HunarSangwanPG@gmail.com>
2024-05-04 12:55:24 +0530

    Added a Main Windowc

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QApplication
+from src.mainWindow import MainWindow
 
 app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
 
 app.exec()
