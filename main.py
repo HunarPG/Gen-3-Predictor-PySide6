04ce57f747ace8a7d6a2ca047ae0f8378221ac51
HunarPG <HunarSangwanPG@gmail.com>
2024-05-04 12:47:48 +0530

    Cleaned Qt Creator code to be simpler

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
 
+app = QApplication(sys.argv)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    # ...
-    sys.exit(app.exec())
+app.exec()
